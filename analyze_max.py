c856133c14ca43504a12f1d42e76ed7dfb09c264
Joe Russack <jrussack@calacademy.org>
2022-09-20 21:19:01 -0700

    analyzer progress..

--- analyze_max.py
+++ analyze_max.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-import sys
-
 from sqlalchemy import create_engine
 
 import pandas as pd
@@ -48,27 +46,20 @@ def get_process_dataframe():
 
     df['snapshot_datetime'] = pd.to_datetime(df['snapshot_datetime'], dayfirst=True)
     df['snapshot_datetime' + str('_date')] = df['snapshot_datetime'].dt.strftime("%m/%d/%y")
-    # df['snapshot_datetime' + str('_day_time')] = \
-    #     df['snapshot_datetime'].dt.day_name() + \
-    #     " " + \
-    #     df['snapshot_datetime'].dt.strftime('%d') + \
-    #     ", " + \
-    #     df['snapshot_datetime'].dt.strftime('%H')
 
     return df
 
 
-def process(df):
-    # ** drop rows where username == args == args == pid == host and
+def show_usage_graph(df, by="username"):
+    # ** drop rows where username == args == pid == host and
     # retain the record with the highest cpu time.
 
+    # Sort so that we choose only the highest value (i.e. final)
+    # cputime. Cputime is cumulative (inherent in the output of linux)
     df_max_process_usage_only = df.sort_values('cputimes', ascending=False). \
         drop_duplicates(['username', 'host', 'pid'])
 
-    df_max_process_usage_only_broken = df.drop_duplicates(['username', 'host', 'pid'])
-
-
-    df_agg2 = df_max_process_usage_only.groupby(['username']). \
+    df_agg = df_max_process_usage_only.groupby([by]). \
         agg({'pid': 'count',
              'cputimes': 'sum',
              'rss': 'sum',
@@ -78,17 +69,55 @@ def process(df):
         reset_index(). \
         rename(columns={'cputimes': 'cputimes_sum', 'pid': 'pid_count'}). \
         sort_values(by='cputimes_sum', ascending=False)
-    print(df_agg2.head())
-    df_agg2 = df_agg2.head(8)
+    # print(df_agg2.head())
+    df_agg = df_agg.head(8)
     start_date = df_max_process_usage_only['snapshot_datetime_date'].min()
     end_date = df_max_process_usage_only['snapshot_datetime_date'].max()
 
     # print(df_agg2.snapshot_datetime.min())
-    fig = px.pie(df_agg2,
+    fig = px.pie(df_agg,
                  values='cputimes_sum',
-                 names='username',
-                 title=f'Total CPU Time consumption, {start_date} - {end_date}')
+                 names=by,
+                 title=f'Total CPU Time consumption by {by}, {start_date} - {end_date}')
     fig.show()
+
+
+def time_series(df):
+    # time_samples = df['snapshot_time_epoch'].unique()
+    # print(f"{time_samples}")
+    time_slots = df.sort_values('snapshot_time_epoch', ascending=True).groupby(['snapshot_time_epoch'])
+    prev_slot = None
+    new_df = df
+    new_df['elapsed_time'] = 0
+    new_df.set_index(['pid','host','snapshot_time_epoch'])
+    for epoch_time, time_slot_epoch in time_slots:
+        if prev_slot is not None:
+            merged = pd.merge(prev_slot[['pid', 'host', 'cputimes']],
+                              time_slot_epoch[['pid', 'host', 'cputimes', 'snapshot_time_epoch']],
+                              how='right',
+                              on=['pid', 'host'],
+                              suffixes=('_prev', '_cur')
+                              )
+            merged['elapsed_time'] = merged['cputimes_cur'] - merged['cputimes_prev']
+            merged.drop('cputimes_cur', axis=1, inplace=True)
+            merged.drop('cputimes_prev', axis=1, inplace=True)
+            merged.set_index(['pid', 'host', 'snapshot_time_epoch'])
+            merged = merged[merged.elapsed_time != 0]
+            # new_df = pd.merge(new_df,
+            #                   merged[['pid', 'snapshot_time_epoch', 'host', 'elapsed_time']],
+            #                   how='right',
+            #                   on=['pid', 'snapshot_time_epoch', 'host']
+            #                   )
+            new_df = new_df.combine_first(merged)
+
+            print(f"df")
+        prev_slot = time_slot_epoch
+
+    print(f"{df.head(5)}")
+    print(f"{df.info()}")
+    # for time_sample in time_samples:
+    #     print(f"time sample: {time_sample}")
+    print(f"done.")
 
 
 # df = get_process_dataframe()
@@ -100,4 +129,6 @@ def process(df):
 dbfile = open('dataframe_pickle.pkl', 'rb')
 df = pickle.load(dbfile)
 print("Pickle loaded...")
-process(df)
+# show_usage_graph(df,'comm')
+# show_usage_graph(df,'username')
+time_series(df)
