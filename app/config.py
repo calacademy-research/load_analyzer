979b1305be752d49ca851b5abfd9c1d5575577ce
shiqi <shiqi.yang@instabase.com>
2024-12-11 23:35:54 -0800

    add date control

---
+++ app/config.py
@@ -0,0 +1,8 @@
+# Database configuration
+DB_CONFIG = {
+    'host': '10.1.10.123',
+    'database': 'load',
+    'user': 'root',
+    'password': 'qhALiqwRFNlOzwqnbXgGbKpgCZXUiSZvmAsRLlFIIMqjSQrf',
+    'port': 3312
+} 
